a77fb923f7f4af1edf14e32f9bb7c64a23723467
Samueldiener <47459400+Samueldiener@users.noreply.github.com>
2019-02-11 13:44:56 -0500

    Add files via upload

---
+++ rtest2v2v2.R
@@ -0,0 +1,69 @@
+library(plotly)
+
+packageVersion('plotly')
+#(devtools::install_github("ropensci/plotly"))
+
+Sys.setenv("plotly_username"="sdiener")
+Sys.setenv("plotly_api_key"="L9C7mwFnERZoAO42jHEz")
+#I added an "authorID" column to the corpusmaster_id doc
+
+#LLcmAuNum <- read.csv("C:/Users/Dienerkins/Desktop/WWP paper/sortingbox/corpusmaster_id_withLL_AuNum.csv")
+LLcmAuNum <- read.csv("C:/Users/Dienerkins/Desktop/WWP paper/sortingbox/corpusmergedbyauthor.csv")
+
+
+
+## THESE ARE NAs???
+LLcmAuNum[is.na(LLcmAuNum$authorID),]
+# drop NAs
+LLcmAuNum <- LLcmAuNum[!is.na(LLcmAuNum$authorID),]
+
+LLcmAuNum <- group_by(LLcmAuNum, authorID)
+
+# geo styling
+g <- list(
+	scope = 'world',
+	projection = list(type = 'mercator'),
+	showland = TRUE,
+	landcolor = toRGB("gray95"),
+	subunitcolor = toRGB("gray85"),
+	countrycolor = toRGB("gray85"),
+	countrywidth = 0.5,
+	subunitwidth = 0.5
+)
+
+# main plot
+p <- plot_geo(LLcmAuNum, 
+              width = 1200,
+              height =800, 
+              x=.4,
+              size = c(1,500)) %>%
+	add_markers(
+		y = ~latitude, x = ~longitude, name= ~author, size = ~count+50, color = I("#1f77b4"),
+		text = ~paste(DisplayName, 
+		              paste("References: ", count), 
+		              paste("References by text: ", textCount), 
+		              sep = "<br />"),
+		symbol = I("circle"), span = I(1), hoverinfo = "text")
+
+p <- add_annotations(p, 
+                     text = "Authors: double-click <br />to select, single <br />click to add to <br /> or remove from <br />selection",
+                     xref = "paper",
+                     yref = "paper",
+                     x = 0.88, 
+                     xanchor = "left",
+                     y = .99,
+                     yanchor = "top",
+                     legendtitle = TRUE,
+                     showarrow = FALSE)
+
+# other info
+p <- colorbar(p, title = "Count") %>%
+	layout(
+		title = paste('Individual Authors: Hover for name and count'), geo = g, showlegend = TRUE 
+	)
+p
+
+# Create a shareable link to your chart
+# Set up API credentials: https://plot.ly/r/getting-started
+chart_link = api_create(p, filename = "Interactive Flat Map with Sizepins 8")
+chart_link
